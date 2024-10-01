e54bc03b4ccc19b919f40f270efbd15d2b1714b7
Bryan Prinz <146386080+Bryanps7@users.noreply.github.com>
2024-10-01 12:29:05 -0300

    v1.0.0

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+const openMenu = document.getElementById('openMenu')
+const closeMenu = document.getElementById('closeMenu')
+const menu = document.getElementById('menu')
+
+openMenu.addEventListener('click', ()=>{
+    menu.style.display = 'flex'
+    menu.style.right = (menu.offsetWidth * -1) + '1px'
+    openMenu.style.display = 'none'
+    setTimeout(()=>{
+        menu.style.opacity = '1'
+
+        menu.style.right = '0'
+    },10)
+})
+
+closeMenu.addEventListener('click', ()=>{
+    menu.style.opacity = '0'
+    menu.style.right = (menu.offsetWidth * -1) + '1px'
+    setTimeout(()=>{
+        menu.removeAttribute('style')
+        openMenu.style.display = 'block'
+    },200)
+})
